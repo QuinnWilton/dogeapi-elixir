d326362c5170ed3362e0e397ab84703c64f00b3b
Shane Wilton <shane.wilton@gmail.com>
2014-02-15 02:13:29 -0500

    Wrap DogeAPI

--- lib/dogeapi.ex
+++ lib/dogeapi.ex
@@ -1,9 +1,64 @@
-defmodule Dogeapi do
-  use Application.Behaviour
+defmodule DogeAPI do
+	use HTTPotion.Base
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    Dogeapi.Supervisor.start_link
-  end
+	def process_url(url) do
+		"https://www.dogeapi.com/wow/?" <> url
+	end
+
+	def get_balance(api_key) do
+		params = [api_key: api_key]
+		construct_url("get_balance", params)
+			|> get
+	end
+
+	def withdraw(api_key, amount, payment_address) do
+		params = [api_key: api_key, amount: amount, payment_address: payment_address]
+		construct_url("withdraw", params)
+			|> get
+	end
+
+	def get_new_address(api_key, address_label) do
+		params = [api_key: api_key, address_label: address_label]
+		construct_url("get_new_address", params)
+			|> get
+	end
+
+	def get_my_addresses(api_key) do
+		params = [api_key: api_key]
+		construct_url("get_my_addresses", params)
+			|> get
+	end
+
+	def get_address_received(api_key, payment_address) do
+		params = [api_key: api_key, payment_address: payment_address]
+		construct_url("get_address_received", params)
+			|> get
+	end
+
+	def get_address_by_label(api_key, address_label) do
+		params = [api_key: api_key, address_label: address_label]
+		construct_url("get_address_by_label", params)
+			|> get
+	end
+
+	def get_difficulty do
+		construct_url("get_difficulty", [])
+			|> get
+	end
+
+	def get_current_block do
+		construct_url("get_current_block", [])
+			|> get
+	end
+
+	def get_current_price do
+		construct_url("get_current_price", [])
+			|> get
+	end
+
+	defp construct_url(action, params) do
+		Enum.reduce params, "a=#{action}&", fn({key, val}, acc) ->
+			acc <> "#{key}=#{val}&"
+		end
+	end
 end
--- test/dogeapi_test.exs
+++ test/dogeapi_test.exs
@@ -1,7 +1,3 @@
-defmodule DogeapiTest do
+defmodule DogeAPITest do
   use ExUnit.Case
-
-  test "the truth" do
-    assert(true)
-  end
 end
